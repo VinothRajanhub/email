34dfb9a7ea6adf99833e3e6c80c269f57928a1e2
Vinoth Rajan <vinoth@alphabsolutions.com>
2020-08-12 23:40:54 +0530

    Task 2 - Get Unique character

--- email.js
+++ email.js
@@ -1,16 +1,16 @@
 // Task 1
 
 var check = 0;
-var emailArray = ["demo@gmail.com", "demo1@gmail.com", "demo2@gmail.com"];
+var emailArray = ["demo@gmail.com", "demo1@gmail.com", "demo2@gmail.com","demo4@gmail.com"];
 
 var registeredEmail = (userEmail) =>{
   check = 0;
   emailArray.forEach((element) => {
-    if (userEmail == element) {
+    if (userEmail === element) {
       check = 1;
     }
   });
-  if (check == 1) {
+  if (check === 1) {
     console.log("Email already exist");
   } else {
     console.log("Signed Up Area");
@@ -19,7 +19,7 @@ var registeredEmail = (userEmail) =>{
 
 
 registeredEmail("demo1@gmail.com");
-registeredEmail("demo4@gmail.com");
+registeredEmail("demo5@gmail.com");
 registeredEmail("demo2@gmail.com");
 
 
@@ -29,29 +29,32 @@ registeredEmail("demo2@gmail.com");
 var email1 = "vinothrajan@gmail.com";
 var splitted = email1.split("@");
 // console.log(splitted); //splitted here
-var splitEmail = splitted[0];
+
+// Get Unique Character
+var splitEmail = [...new Set(splitted[0])].join('');
+
+console.log(splitEmail,typeof(splitEmail))
 
 var forty = (splitted[0].length * 40) / 100;
-// console.log(Math.floor(forty));
+var fortyPercent = Math.floor(forty);
 
 var counter = 0;
 
 var password = "rajan";
 // var password = "ra";
 // var password = "razz";
+// var password = "irva";
 var arrPwd = password;
 
 for (let i = 0; i < splitEmail.length; i++) {
-  let nameStart = i;
   for (let j = 0; j < arrPwd.length; j++) {
-    if (splitEmail[nameStart] === arrPwd[j]) {
+    if (splitEmail[i] === arrPwd[j]) {
       counter++;
     }
-    nameStart++;
   }
 }
 
-if (forty < counter) {
+if (fortyPercent < counter) {
   console.log("You cannot signup with email with this password");
 } else {
   console.log("Signed Up Successfully");
